893167f93408117e1e7dd999ae2377114db30850
shawshaenk <138606372+shawshaenk@users.noreply.github.com>
2023-07-19 22:04:06 -0500

    Add files via upload

--- desnapper.sh
+++ desnapper.sh
@@ -4,7 +4,7 @@ newline=$'\n'
 snap_packages=$(snap list | awk 'NR > 1 {print $1}')
 
 installed_packages() {
-    IFS=' ' read -r -a snap_packages_list <<< "$snap_packages"
+    readarray -t snap_packages_list <<<"$x"
     for num in 1 3; do
         for snap in "${snap_packages_list[@]}"; do
             if [ "$snap" != "snapd" ]; then 
