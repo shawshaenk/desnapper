3e5dcdb1d393beafe44c829a1e9f5e75022aaaeb
shawshaenk <138606372+shawshaenk@users.noreply.github.com>
2023-07-21 19:56:45 -0500

    Add files via upload

--- desnapper.sh
+++ desnapper.sh
@@ -50,10 +50,6 @@ purge_snaps() {
         sudo apt install --install-suggests gnome-software -y
     fi
 
-    echo "deb http://packages.linuxmint.com victoria upstream" | sudo tee /etc/apt/sources.list.d/mint-vera.list
-    sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com A1715D88E1DF1F24 40976EAF437D05B5 3B4FE6ACC0B21F32 A6616109451BBBF2
-    sudo apt update
-
     echo "${newline}All snaps packages have been purged!"
 }
 
